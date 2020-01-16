70ecff89d60791ce177b78ca46648b256f2d024e
nmarini <nmarini44@gmail.com>
2020-01-15 17:02:03 -0800

    removed className

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ class List extends Component {
     render() {
         return (
             <>
-            {this.props.items.map(item => <button key={item.title || item.name} onClick={this.props.handleClick}>{item.title || item.name}</button>) }
+            {this.props.items.map(item => <button className="info" key={item.title || item.name} onClick={this.props.handleClick}>{item.title || item.name}</button>) }
             </>
         )
     }
