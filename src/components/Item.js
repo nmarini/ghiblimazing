705aef8a32ee654ddfb0d2ce0cc45f2e1dc5d139
nmarini <nmarini44@gmail.com>
2020-01-21 16:15:57 -0800

    refactoring what info is shown when item is presented - specifically including films names when relevant

--- src/components/Item.js
+++ src/components/Item.js
@@ -13,8 +13,15 @@ class Item extends Component {
 
                 {this.itemProperties().map((prop, index) => {
                     if (prop[0] !== this.itemTitle()[0] && prop[0] !== 'id') {
-                        if (prop[1].toString().includes("http")) {
-                            return <li key={index}><a href={prop[1]} target="_blank" rel="noopener noreferrer"><b>{prop[0]}</b></a></li>
+                        if (prop[1].toString().includes("http") && prop[1].toString().includes("films")) {
+                            
+                            fetch(`${prop[1]}`)
+                                .then(res => res.json())
+                                .then((result) => {
+                                    console.log(result)
+                                })
+                                // return <li key={index}></li>
+                            // return <li key={index}><a href={prop[1]} target="_blank" rel="noopener noreferrer"><b>{prop[0]}</b></a></li>
                         } else {
                             return <li key={index}><b>{prop[0].toUpperCase()}</b>: <em>{prop[1]}</em></li> 
                         }
@@ -28,3 +35,12 @@ class Item extends Component {
 }
 
   export default Item;
+
+//   fetchAPI = (items) => {
+//     fetch(`https://ghibliapi.herokuapp.com/${items}`)
+//         .then(res => res.json())
+//         .then((result) => {
+//             this.setState({items: result});
+//         }   
+//     )
+// }
